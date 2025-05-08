8cced853e92e1136040c2fe2585a2c1eb16e3fec
Eli-H20 <elijah.hamel24@student.cart.org>
2025-05-08 10:42:07 -0700

    css

---
+++ script.js
@@ -0,0 +1,9 @@
+function makeGone() { 
+	document.getElementById("begone").style.display="none"; 
+	document.getElementById("come").style.display="block"; 
+}
+
+function comeBack() { 
+	document.getElementById("begone").style.display="block"; 
+	document.getElementById("come").style.display="none"; 
+}
